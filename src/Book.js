038e2314554425c2c714b26f728cd8bd492a85e6
Sophie <sophie.nye@gmail.com>
2019-07-05 21:46:28 -0400

    styling added

--- src/Book.js
+++ src/Book.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import "./App.css";
+import "./Book.css";
+import Draggable from 'react-draggable'
 
 class Book extends Component {
 
@@ -7,10 +9,21 @@ class Book extends Component {
     render(props) {
         console.log('book props ', props)
         return (
-            <div>
-                <img src={this.props.cover}></img>
-                <p>{this.props.title}</p>
-            </div>
+            <Draggable
+                axis="x"
+                handle=".handle"
+                defaultPosition={{ x: 0, y: 0 }}
+                position={null}
+                grid={[25, 25]}
+                scale={1}
+                onStart={this.handleStart}
+                onDrag={this.handleDrag}
+                onStop={this.handleStop}>
+                <div className="book">
+                    <img src={this.props.cover}></img>
+                    <p>{this.props.title}</p>
+                </div>
+            </Draggable>
         )
     }
 
