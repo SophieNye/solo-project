5515127392dd7898ff1a3efac96bb6519fdd5958
Sophie <sophie.nye@gmail.com>
2019-09-18 15:06:30 -0400

    added first test

--- __tests__/supertest.js
+++ __tests__/supertest.js
@@ -3,3 +3,16 @@ const fs = require('fs');
 const path = require('path');
 
 const server = 'http://localhost:3000';
+
+describe('Route integration', () => {
+    describe('/', () => {
+      describe('GET', () => {
+        it('responds with 200 status and text/html content type', () => {
+          return request(server)
+            .get('/')
+            .expect('Content-Type', /text\/html/)
+            .expect(200);
+        });
+      });
+    });
+});
